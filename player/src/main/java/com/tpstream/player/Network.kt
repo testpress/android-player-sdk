3e758a46c47698b6a3982b2ea22de70dd310f60b
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-11-09 18:15:22 +0530

    Add throwable and catch exception

--- player/src/main/java/com/tpstream/player/Network.kt
+++ player/src/main/java/com/tpstream/player/Network.kt
@@ -36,8 +36,9 @@ class Network<T : Any>(val klass: Class<T>, val subdomain: String) {
         val response = client.newCall(request).execute()
         if (response.isSuccessful) {
             return gson.fromJson(response.body?.charStream(), klass)
+        } else{
+            throw TPException.httpError(response)
         }
-        return null
     }
 
     fun get(url: String, callback: TPResponse<T>? = null): T? {

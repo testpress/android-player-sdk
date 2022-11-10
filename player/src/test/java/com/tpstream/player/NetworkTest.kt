753995c6e9eb4c463693875166c26c9787c27f0f
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-11-10 19:34:14 +0530

    Add test case to sync and async request

--- player/src/test/java/com/tpstream/player/NetworkTest.kt
+++ player/src/test/java/com/tpstream/player/NetworkTest.kt
@@ -3,6 +3,9 @@ package com.tpstream.player
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.tpstream.player.models.DRMLicenseURL
 import com.tpstream.player.models.VideoInfo
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import okhttp3.internal.EMPTY_REQUEST
 import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
@@ -67,21 +70,66 @@ class NetworkTest {
         }
     }
 
-    private fun getVideoInfoJSON(): String {
-        return """
-             {
-    "title": "Big bunny video",
-    "thumbnail": null,
-    "thumbnail_small": null,
-    "thumbnail_medium": null,
-    "url": "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/",
-    "dash_url": "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/video.mpd",
-    "hls_url": "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/video.m3u8",
-    "duration": "0:10:34",
-    "description": "",
-    "transcoding_status": 4
-}
-        """.trimIndent()
+    @Test
+    fun testGetVideoInfoWithAsyncRequestOnSuccess() {
+        val successResponse = MockResponse().setResponseCode(200).setBody(getVideoInfoJSON())
+        mockWebServer.enqueue(successResponse)
+        var response: VideoInfo?
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                response = networkVideoInfo.get(mockWebServer.url("/").toString(),
+                    object : Network.TPResponse<VideoInfo> {
+                        override fun onSuccess(result: VideoInfo) {
+                            response = result
+                        }
+
+                        override fun onFailure(exception: TPException) {
+
+                        }
+
+                    })
+                Thread.sleep(500)
+            }
+            mockWebServer.takeRequest()
+        }
+        assertEquals(
+            response?.dashUrl,
+            "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/video.mpd"
+        )
+        assertEquals(
+            response?.hlsUrl,
+            "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/video.m3u8"
+        )
+        assertEquals(
+            response?.url,
+            "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/"
+        )
+    }
+
+    @Test
+    fun testGetVideoInfoWithAsyncRequestOnFailure() {
+        val successResponse = MockResponse().setResponseCode(401)
+        mockWebServer.enqueue(successResponse)
+        var tpException: TPException? = null
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                val response = networkVideoInfo.get(mockWebServer.url("/").toString(),
+                    object : Network.TPResponse<VideoInfo> {
+                        override fun onSuccess(result: VideoInfo) {
+
+                        }
+
+                        override fun onFailure(exception: TPException) {
+                            tpException = exception
+                        }
+
+                    })
+                Thread.sleep(500)
+            }
+            mockWebServer.takeRequest()
+        }
+        assertEquals(tpException?.response?.code, 401)
+        assertEquals(tpException?.isUnauthenticated(), true)
 
     }
 
@@ -109,6 +157,77 @@ class NetworkTest {
             assertEquals(exception.isUnauthenticated(), true)
             assertEquals(exception.response?.code, 401)
         }
+    }
+
+    @Test
+    fun testPostDRmLicenseWithAsyncRequestOnSuccess() {
+        val successResponse = MockResponse().setResponseCode(200).setBody(getDRMLicenseJSON())
+        mockWebServer.enqueue(successResponse)
+        var response: DRMLicenseURL?
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                response = networkLicense.post(mockWebServer.url("/").toString(), EMPTY_REQUEST,
+                    object : Network.TPResponse<DRMLicenseURL> {
+                        override fun onSuccess(result: DRMLicenseURL) {
+                            response = result
+                        }
+
+                        override fun onFailure(exception: TPException) {
+
+                        }
+
+                    })
+                Thread.sleep(500)
+            }
+            mockWebServer.takeRequest()
+        }
+        assertEquals(
+            response?.licenseUrl,
+            "https://drm.testpress.in/api/v2.5/chapter_contents/drm_license_key/?path=wv/eyJjb250ZW50QXV0aCI6ImV5SmpiMjUwWlc1MFNXUWlPaUkyT1dVeU1qTTRNV00xWXpRMFpqbGxZVGt6WldRNE1qaG1NekZtWWprM01TSXNJbVY0Y0dseVpYTWlPakUyTmpnd056VTVOemg5Iiwic2lnbmF0dXJlIjoiVm9oVWJRR1g2d1p0ZWxhSjoyMDIyMTExMFQwOTI2MTg5ODlaOmh5cV8yNmFRcm13a1lvN3ZJdVJad2M2U2dVSE9OOWFwTzVrTW5KOTR6aTg9In0=&v=1"
+        )
+    }
+
+    @Test
+    fun testPostDRmLicenseWithAsyncRequestOnFailure() {
+        val successResponse = MockResponse().setResponseCode(401)
+        mockWebServer.enqueue(successResponse)
+        var tpException: TPException? = null
+        runBlocking {
+            withContext(Dispatchers.IO) {
+                val response = networkLicense.post(mockWebServer.url("/").toString(), EMPTY_REQUEST,
+                    object : Network.TPResponse<DRMLicenseURL> {
+                        override fun onSuccess(result: DRMLicenseURL) {
+                        }
+
+                        override fun onFailure(exception: TPException) {
+                            tpException = exception
+                        }
+
+                    })
+                Thread.sleep(500)
+            }
+            mockWebServer.takeRequest()
+        }
+        assertEquals(tpException?.isUnauthenticated(), true)
+        assertEquals(tpException?.response?.code, 401)
+    }
+
+    private fun getVideoInfoJSON(): String {
+        return """
+             {
+    "title": "Big bunny video",
+    "thumbnail": null,
+    "thumbnail_small": null,
+    "thumbnail_medium": null,
+    "url": "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/",
+    "dash_url": "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/video.mpd",
+    "hls_url": "https://dyik8aq8drlno.cloudfront.net/institute/drm/courses/drm-encrypted-video/videos/transcoded/97912f63a2ad4e059bddbe2eb51fe3f6/video.m3u8",
+    "duration": "0:10:34",
+    "description": "",
+    "transcoding_status": 4
+}
+        """.trimIndent()
+
     }
 
     private fun getDRMLicenseJSON(): String {

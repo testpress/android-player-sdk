f650a607389f77e77e058aba3b2c27f23529ba9f
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-11-24 20:35:52 +0530

    temp

--- player/src/androidTest/java/com/tpstream/player/views/DownloadResolutionSelectionSheetTest.kt
+++ player/src/androidTest/java/com/tpstream/player/views/DownloadResolutionSelectionSheetTest.kt
@@ -1,23 +1,12 @@
 package com.tpstream.player.views
 
-import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.lifecycle.Lifecycle
-import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
-import androidx.test.core.app.ApplicationProvider
-import androidx.test.espresso.Espresso
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.MediumTest
-import androidx.test.rule.ActivityTestRule
 import com.tpstream.player.TpStreamPlayer
 import com.tpstream.player.TpStreamPlayerFragment
-import org.junit.After
-import org.junit.Before
-import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
@@ -32,41 +21,40 @@ class DownloadResolutionSelectionSheetTest {
 
     private var dialogFragment: DownloadResolutionSelectionSheet? = null
 
-    @Rule
-    val activityTestRule: ActivityTestRule<TestActivity> =
-        ActivityTestRule(TestActivity::class.java)
+//    @Rule
+//    val activityTestRule: ActivityTestRule<TestActivity> =
+//        ActivityTestRule(TestActivity::class.java)
 
 
-    @Before
-    fun showDialog() {
-        // Create a dismiss listener to check with verifyDialogWasDismissed().
-        dialogFragment = DownloadResolutionSelectionSheet(
-            player,
-            DefaultTrackSelector.Parameters.Builder(
-                ApplicationProvider.getApplicationContext<Context?>().applicationContext
-            )
-                .build(),
-            player.getCurrentTrackGroups()
-        )
-        dialogFragment!!.show(activityTestRule.activity.supportFragmentManager, "AdvancedSheetDownload")
-
-        // Wait for dialog to be shown.
-        onView(withText("Download Quality")).check(matches(isDisplayed()))
-    }
-
-    @After
-    fun tearDown() {
-        if (dialogFragment != null && dialogFragment!!.dialog != null && dialogFragment!!.dialog!!.isShowing
-        ) {
-            // Close the dialog
-            Espresso.pressBack()
-        }
-    }
+//    @Before
+//    fun showDialog() {
+//        // Create a dismiss listener to check with verifyDialogWasDismissed().
+//        dialogFragment = DownloadResolutionSelectionSheet(
+//            player,
+//            DefaultTrackSelector.Parameters.Builder(
+//                ApplicationProvider.getApplicationContext<Context?>().applicationContext
+//            )
+//                .build(),
+//            player.getCurrentTrackGroups()
+//        )
+//        //dialogFragment!!.show(activityTestRule.activity.supportFragmentManager, "AdvancedSheetDownload")
+//
+//        // Wait for dialog to be shown.
+//        onView(withText("Download Quality")).check(matches(isDisplayed()))
+//    }
+//
+//    @After
+//    fun tearDown() {
+//        if (dialogFragment != null && dialogFragment!!.dialog != null && dialogFragment!!.dialog!!.isShowing
+//        ) {
+//            // Close the dialog
+//            Espresso.pressBack()
+//        }
+//    }
 
     @Test
     fun a() {
-        val scenario = launchFragmentInContainer<TpStreamPlayerFragment>(Bundle())
-        scenario.moveToState(Lifecycle.State.STARTED)
+        launchFragmentInContainer<TpStreamPlayerFragment>(Bundle())
 //        scenario.withFragment {
 //            dialogFragment = DownloadResolutionSelectionSheet(
 //                player,
@@ -81,7 +69,7 @@ class DownloadResolutionSelectionSheetTest {
         // Wait for dialog to be shown.
 
         // Wait for dialog to be shown.
-        onView(withText("Download Quality")).check(matches(isDisplayed()))
+        //onView(withText("Download Quality")).check(matches(isDisplayed()))
     }
 
 //    @Test

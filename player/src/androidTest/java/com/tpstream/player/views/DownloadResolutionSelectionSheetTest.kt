ad57acad76f52737ad4f5c3599eec19642a3729e
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-11-25 10:28:35 +0530

    temp

--- player/src/androidTest/java/com/tpstream/player/views/DownloadResolutionSelectionSheetTest.kt
+++ player/src/androidTest/java/com/tpstream/player/views/DownloadResolutionSelectionSheetTest.kt
@@ -2,17 +2,16 @@ package com.tpstream.player.views
 
 import android.os.Bundle
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.lifecycle.Lifecycle
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.MediumTest
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.tpstream.player.TpStreamPlayer
-import com.tpstream.player.TpStreamPlayerFragment
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.mockito.Mock
 
 
-@MediumTest
 @RunWith(AndroidJUnit4::class)
 class DownloadResolutionSelectionSheetTest {
 
@@ -21,61 +20,18 @@ class DownloadResolutionSelectionSheetTest {
 
     private var dialogFragment: DownloadResolutionSelectionSheet? = null
 
-//    @Rule
-//    val activityTestRule: ActivityTestRule<TestActivity> =
-//        ActivityTestRule(TestActivity::class.java)
-
-
-//    @Before
-//    fun showDialog() {
-//        // Create a dismiss listener to check with verifyDialogWasDismissed().
-//        dialogFragment = DownloadResolutionSelectionSheet(
-//            player,
-//            DefaultTrackSelector.Parameters.Builder(
-//                ApplicationProvider.getApplicationContext<Context?>().applicationContext
-//            )
-//                .build(),
-//            player.getCurrentTrackGroups()
-//        )
-//        //dialogFragment!!.show(activityTestRule.activity.supportFragmentManager, "AdvancedSheetDownload")
-//
-//        // Wait for dialog to be shown.
-//        onView(withText("Download Quality")).check(matches(isDisplayed()))
-//    }
-//
-//    @After
-//    fun tearDown() {
-//        if (dialogFragment != null && dialogFragment!!.dialog != null && dialogFragment!!.dialog!!.isShowing
-//        ) {
-//            // Close the dialog
-//            Espresso.pressBack()
-//        }
-//    }
+    //private val bundle  = Bundle().
 
     @Test
-    fun a() {
-        launchFragmentInContainer<TpStreamPlayerFragment>(Bundle())
-//        scenario.withFragment {
-//            dialogFragment = DownloadResolutionSelectionSheet(
-//                player,
-//                DefaultTrackSelector.Parameters.Builder(ApplicationProvider.getApplicationContext<Context?>().applicationContext)
-//                    .build(), player.getCurrentTrackGroups()
-//            )
-//            dialogFragment!!.show(
-//                this.childFragmentManager.beginTransaction(),
-//                "AdvancedSheetDownload"
-//            )
-//        }
-        // Wait for dialog to be shown.
+    fun launchUi() {
 
-        // Wait for dialog to be shown.
-        //onView(withText("Download Quality")).check(matches(isDisplayed()))
+        launchFragmentInContainer<BottomSheetDialogFragment>().let { scenario ->
+            scenario
+                .moveToState(Lifecycle.State.RESUMED)
+                .onFragment { fragment ->
+                    fragment.show(fragment.parentFragmentManager.beginTransaction(),"tag")
+
+                }
+        }
     }
-
-//    @Test
-//    fun test_player_view_is_visible() {
-//
-//        onView(withText("Download Quality")).inRoot(isDialog())
-//    }
-
 }

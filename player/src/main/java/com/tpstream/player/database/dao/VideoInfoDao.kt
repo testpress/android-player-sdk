93e5bc8fafe38a2c8d7e91dbc31da9d875d51000
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-11-25 17:26:24 +0530

    Refactor

--- player/src/main/java/com/tpstream/player/database/dao/VideoInfoDao.kt
+++ player/src/main/java/com/tpstream/player/database/dao/VideoInfoDao.kt
@@ -1,28 +1,21 @@
 package com.tpstream.player.database.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.tpstream.player.models.VideoInfo
 
 @Dao
 interface VideoInfoDao {
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(videoInfo: VideoInfo)
 
     @Delete
     fun delete(videoInfo: VideoInfo)
 
-    @Query("SELECT * FROM videoinfo WHERE url=:url")
-    fun getByUrl(url: String): VideoInfo?
-
-    @Query("SELECT * FROM videoinfo WHERE dashUrl=:dashUrl")
-    fun getByDashUrl(dashUrl: String): VideoInfo?
-
-    @Query("SELECT * FROM videoinfo ORDER BY title ASC")
+    @Query("SELECT * FROM videoinfo")
     fun getAllVideoInfo():List<VideoInfo>
 
     @Query("SELECT * FROM videoinfo WHERE videoId=:videoID")
     fun getVideoInfoByVideoId(videoID:String):VideoInfo?
-
 
 }

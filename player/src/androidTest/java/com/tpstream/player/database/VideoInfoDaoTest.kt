6ff39538f734c6d0399e2aa293ae4ee3fbe66871
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-11-28 16:11:06 +0530

    refactor

--- player/src/androidTest/java/com/tpstream/player/database/VideoInfoDaoTest.kt
+++ player/src/androidTest/java/com/tpstream/player/database/VideoInfoDaoTest.kt
@@ -4,33 +4,15 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.tpstream.player.models.VideoInfo
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Assert
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
 
-
 @RunWith(AndroidJUnit4::class)
-class VideoInfoDaoTest :TPStreamsDatabaseTest(){
-
-    private fun createData(): VideoInfo {
-        return VideoInfo(
-            "Test",
-            "Demo",
-            null,
-            null,
-            null,
-            "url",
-            "dashUrl",
-            "hslUrl",
-            null,
-            null,
-            null
-        )
-    }
+class VideoInfoDaoTest : TPStreamsDatabaseTest() {
 
     @Test
-    fun testAddData(){
+    fun testAddData() {
         val videoInfo = createData()
         db.videoInfoDao().insert(videoInfo)
 
@@ -39,7 +21,7 @@ class VideoInfoDaoTest :TPStreamsDatabaseTest(){
     }
 
     @Test
-    fun testDeleteData(){
+    fun testDeleteData() {
         val videoInfo = createData()
         db.videoInfoDao().insert(videoInfo)
 
@@ -51,10 +33,39 @@ class VideoInfoDaoTest :TPStreamsDatabaseTest(){
     }
 
     @Test
-    fun testGetVideoInfoByVideoId(){
+    fun testGetVideoInfoByVideoId() {
         val videoInfo = createData()
         db.videoInfoDao().insert(videoInfo)
 
         assertThat(db.videoInfoDao().getVideoInfoByVideoId("Test"), equalTo(videoInfo))
+    }
+
+    @Test
+    fun testGetAllVideoInfo() {
+        val data1 = createData("1")
+        val data2 = createData("2")
+        val data3 = createData("3")
+
+        db.videoInfoDao().insert(data1)
+        db.videoInfoDao().insert(data2)
+        db.videoInfoDao().insert(data3)
+
+        assertThat(db.videoInfoDao().getAllVideoInfo().size, equalTo(3))
+    }
+
+    private fun createData(count: String = ""): VideoInfo {
+        return VideoInfo(
+            "Test$count",
+            "Demo",
+            null,
+            null,
+            null,
+            "url",
+            "dashUrl",
+            "hslUrl",
+            null,
+            null,
+            null
+        )
     }
 }

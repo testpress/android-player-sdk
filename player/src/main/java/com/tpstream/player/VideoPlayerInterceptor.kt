1c895cc3645908cc90cdd530a3f9830396f0d1fb
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-12-08 09:52:43 +0530

    Refactor

--- player/src/main/java/com/tpstream/player/VideoPlayerInterceptor.kt
+++ player/src/main/java/com/tpstream/player/VideoPlayerInterceptor.kt
@@ -11,7 +11,7 @@ class VideoPlayerInterceptor(val context: Context) : Interceptor {
         var request = chain.request()
 
         if(request.url.toString().contains("encryption_key")) {
-            request = request.newBuilder().addHeader("Authorization", "JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6MSwidXNlcl9pZCI6MSwiaW5zdGl0dXRlIjoxNTg1LCJpZCI6MSwiZXhwIjoxNjcwNDI3NzYwLCJlbWFpbCI6InN1cHBvcnRAdmVyYW5kYXJhY2UuY29tIn0.2ChALE2LK7YeNfe_1sqCAEwZp2XTEW6VUab8Jb6dTGw").build()
+            request = request.newBuilder().addHeader("Authorization", "token").build()
         }
         return chain.proceed(request)
     }

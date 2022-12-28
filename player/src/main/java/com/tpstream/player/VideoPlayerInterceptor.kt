7e26e43012634fb75f1b4ab642bcd06c58044e89
PruthiviRaj27 <pruthiviraj@testpress.in>
2022-12-28 12:53:56 +0530

    Refactor

--- player/src/main/java/com/tpstream/player/VideoPlayerInterceptor.kt
+++ player/src/main/java/com/tpstream/player/VideoPlayerInterceptor.kt
@@ -1,7 +1,6 @@
 package com.tpstream.player
 
 import android.content.Context
-import android.util.Log
 import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.Protocol

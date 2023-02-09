0939890fc65e844d090298939603dfed405a113b
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-02-09 14:32:08 +0530

    refactor

--- player/src/main/java/com/tpstream/player/TpStreamPlayerFragment.kt
+++ player/src/main/java/com/tpstream/player/TpStreamPlayerFragment.kt
@@ -19,6 +19,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.media3.common.*
+import androidx.media3.common.PlaybackException.ERROR_CODE_DRM_LICENSE_EXPIRED
 import androidx.media3.common.util.Util
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.analytics.AnalyticsListener
@@ -32,6 +33,7 @@ import com.tpstream.player.views.DownloadResolutionSelectionSheet
 import com.tpstream.player.views.ResolutionOptions
 import com.tpstream.player.views.SimpleVideoResolutionSelectionSheet
 import com.tpstream.player.views.Util.getRendererIndex
+import io.sentry.Sentry
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch

99169c4e3ea7ac616d690ccafaf36635b8e9fa95
PruthiviRaj27 <112844543+PruthiviRaj27@users.noreply.github.com>
2023-02-14 12:00:34 +0530

    refactor: Refactor TPStreamsPlayerFragment (#48)
    Refactor TPStreamsPlayerFragment by stepdown rule

--- player/src/main/java/com/tpstream/player/TpStreamPlayerFragment.kt
+++ player/src/main/java/com/tpstream/player/TpStreamPlayerFragment.kt
@@ -82,26 +82,6 @@ class TpStreamPlayerFragment : Fragment(), DownloadCallback.Listener {
         }).get(OfflineVideoInfoViewModel::class.java)
     }
 
-    fun enableAutoFullScreenOnRotate() {
-        orientationEventListener = OrientationListener(requireContext())
-        orientationEventListener.setOnChangeListener { isLandscape ->
-            activity?.let {
-                if(isLandscape) {
-                    showFullScreen()
-                } else {
-                    exitFullScreen()
-                }
-            }
-        }
-        orientationEventListener.start()
-    }
-
-    fun disableAutoFullScreenOnRotate() {
-        if(::orientationEventListener.isInitialized) {
-            orientationEventListener.disable()
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -117,47 +97,36 @@ class TpStreamPlayerFragment : Fragment(), DownloadCallback.Listener {
         DownloadCallback.invoke().callback = this
     }
 
-    private fun updateDownloadButtonImage(params: TpInitParams){
-        downloadButton = viewBinding.videoView.findViewById(R.id.exo_download)
-        addDownloadControls()
-        if (showDownloadButton){
-            downloadButton.visibility = View.VISIBLE
-        } else {
-            downloadButton.visibility = View.GONE
-        }
-        offlineVideoInfoViewModel.get(params.videoId!!).observe(viewLifecycleOwner) { offlineVideoInfo ->
-            downloadState = when (offlineVideoInfo?.downloadState) {
-                OfflineVideoState.DOWNLOADING ->{
-                    downloadButton.setImageResource(R.drawable.ic_baseline_downloading_24)
-                    OfflineVideoState.DOWNLOADING
-                }
-                OfflineVideoState.COMPLETE ->{
-                    downloadButton.setImageResource(R.drawable.ic_baseline_file_download_done_24)
-                    OfflineVideoState.COMPLETE
-                }
-                else -> {
-                    downloadButton.setImageResource(R.drawable.ic_baseline_download_for_offline_24)
-                    null
-                }
-            }
+    override fun onStart() {
+        super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
         }
     }
 
-    override fun onDownloadsSuccess(videoId:String?) {
-        if (videoId == player?.params?.videoId){
-            reloadVideo()
+    override fun onResume() {
+        super.onResume()
+        if (Util.SDK_INT <= 23 || player == null) {
+            initializePlayer()
         }
-    }
-
-    private fun reloadVideo(){
-        val currentPosition = player?.getCurrentTime()
-        val tpImp = player as TpStreamPlayerImpl
-        tpImp.load(player?.videoInfo?.getPlaybackURL()!!,currentPosition!!)
+        hideSystemUi()
     }
 
     private fun addCustomPlayerControls() {
         addResolutionChangeControl()
         addFullScreenControl()
+    }
+
+    private fun addResolutionChangeControl() {
+        resolutionButton = viewBinding.videoView.findViewById<ImageButton>(R.id.exo_resolution)
+        resolutionButton.setOnClickListener {
+            if (downloadState == OfflineVideoState.COMPLETE){
+                Toast.makeText(requireContext(),"Quality Unavailable",Toast.LENGTH_SHORT).show()
+            } else {
+                val simpleVideoResolutionSelector = initializeVideoResolutionSelectionSheets()
+                simpleVideoResolutionSelector.show(requireActivity().supportFragmentManager, SimpleVideoResolutionSelectionSheet.TAG)
+            }
+        }
     }
 
     private fun addFullScreenControl() {
@@ -170,33 +139,15 @@ class TpStreamPlayerFragment : Fragment(), DownloadCallback.Listener {
         }
     }
 
-    fun exitFullScreen() {
-        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
-        (viewBinding.videoView.parent as ViewGroup).removeView(viewBinding.videoView)
-        viewBinding.mainFrameLayout.addView(viewBinding.videoView)
-        viewBinding.videoView.findViewById<ImageButton>(R.id.fullscreen).setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_fullscreen_24));
-        fullScreenDialog.dismiss()
-        isFullScreen = false
-    }
-
-    fun showFullScreen() {
-        (viewBinding.videoView.parent as ViewGroup).removeView(viewBinding.videoView)
-        fullScreenDialog.addContentView(viewBinding.videoView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
-        viewBinding.videoView.findViewById<ImageButton>(R.id.fullscreen).setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_fullscreen_exit_24));
-        fullScreenDialog.show()
-        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-        isFullScreen = true
-    }
-
-    private fun addResolutionChangeControl() {
-        resolutionButton = viewBinding.videoView.findViewById<ImageButton>(R.id.exo_resolution)
-        resolutionButton.setOnClickListener {
-            if (downloadState == OfflineVideoState.COMPLETE){
-                Toast.makeText(requireContext(),"Quality Unavailable",Toast.LENGTH_SHORT).show()
-            } else {
-                val simpleVideoResolutionSelector = initializeVideoResolutionSelectionSheets()
-                simpleVideoResolutionSelector.show(requireActivity().supportFragmentManager, SimpleVideoResolutionSelectionSheet.TAG)
+    private fun hideSystemUi() {
+        activity?.let { activity ->
+            WindowCompat.setDecorFitsSystemWindows(activity.window, false)
+            WindowInsetsControllerCompat(activity.window, viewBinding.videoView).let { controller ->
+                controller.hide(WindowInsetsCompat.Type.systemBars())
+                controller.systemBarsBehavior =
+                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
             }
+            Log.d(TAG, "hideSystemUi: ")
         }
     }
 
@@ -251,6 +202,91 @@ class TpStreamPlayerFragment : Fragment(), DownloadCallback.Listener {
             }
         }
 
+    fun exitFullScreen() {
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED
+        (viewBinding.videoView.parent as ViewGroup).removeView(viewBinding.videoView)
+        viewBinding.mainFrameLayout.addView(viewBinding.videoView)
+        viewBinding.videoView.findViewById<ImageButton>(R.id.fullscreen).setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_fullscreen_24));
+        fullScreenDialog.dismiss()
+        isFullScreen = false
+    }
+
+    fun showFullScreen() {
+        (viewBinding.videoView.parent as ViewGroup).removeView(viewBinding.videoView)
+        fullScreenDialog.addContentView(viewBinding.videoView, ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT))
+        viewBinding.videoView.findViewById<ImageButton>(R.id.fullscreen).setImageDrawable(ContextCompat.getDrawable(requireContext(), R.drawable.ic_baseline_fullscreen_exit_24));
+        fullScreenDialog.show()
+        requireActivity().requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        isFullScreen = true
+    }
+
+    private fun initializePlayer() {
+        _player = initializeExoplayer()
+        player = TpStreamPlayerImpl(_player!!, requireContext())
+        this.initializationListener?.onInitializationSuccess(player!!)
+    }
+
+    private fun initializeExoplayer(): ExoPlayer {
+        return ExoPlayer.Builder(requireActivity())
+            .setTrackSelector(trackSelector)
+            .build()
+            .also { exoPlayer ->
+                viewBinding.videoView.player = exoPlayer
+                exoPlayer.setAudioAttributes(AudioAttributes.DEFAULT, true)
+                exoPlayer.addListener(_playbackStateListener)
+            }
+    }
+
+    fun setOnInitializationListener(listener: InitializationListener) {
+        this.initializationListener = listener
+    }
+
+    fun load(parameters: TpInitParams) {
+        if (player == null) {
+            throw Exception("Player is not initialized yet. `load` method should be called onInitializationSuccess")
+        }
+        if (startPosition != -1L){
+            parameters.startAt = startPosition
+        }
+
+        player?.load(parameters) { exception ->
+            requireActivity().runOnUiThread {
+                viewBinding.errorMessage.visibility = View.VISIBLE
+                viewBinding.errorMessage.text = "Error Occurred while playing video. Error code ${exception.errorMessage}.\n ID: ${parameters.videoId}"
+                SentryLogger.logAPIException(exception,parameters)
+            }
+        }
+        player?.setPlayWhenReady(parameters.autoPlay==true)
+        showDownloadButton = parameters.isDownloadEnabled
+        updateDownloadButtonImage(parameters)
+    }
+
+    private fun updateDownloadButtonImage(params: TpInitParams){
+        downloadButton = viewBinding.videoView.findViewById(R.id.exo_download)
+        addDownloadControls()
+        if (showDownloadButton){
+            downloadButton.visibility = View.VISIBLE
+        } else {
+            downloadButton.visibility = View.GONE
+        }
+        offlineVideoInfoViewModel.get(params.videoId!!).observe(viewLifecycleOwner) { offlineVideoInfo ->
+            downloadState = when (offlineVideoInfo?.downloadState) {
+                OfflineVideoState.DOWNLOADING ->{
+                    downloadButton.setImageResource(R.drawable.ic_baseline_downloading_24)
+                    OfflineVideoState.DOWNLOADING
+                }
+                OfflineVideoState.COMPLETE ->{
+                    downloadButton.setImageResource(R.drawable.ic_baseline_file_download_done_24)
+                    OfflineVideoState.COMPLETE
+                }
+                else -> {
+                    downloadButton.setImageResource(R.drawable.ic_baseline_download_for_offline_24)
+                    null
+                }
+            }
+        }
+    }
+
     private fun addDownloadControls() {
         downloadButton.setOnClickListener {
             when (downloadState) {
@@ -281,62 +317,20 @@ class TpStreamPlayerFragment : Fragment(), DownloadCallback.Listener {
         }
     }
 
-    fun setOnInitializationListener(listener: InitializationListener) {
-        this.initializationListener = listener
-    }
-
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _viewBinding = null
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        disableAutoFullScreenOnRotate()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        if (Util.SDK_INT > 23) {
-            initializePlayer()
+    override fun onDownloadsSuccess(videoId:String?) {
+        if (videoId == player?.params?.videoId){
+            reloadVideo()
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        if (Util.SDK_INT <= 23 || player == null) {
-            initializePlayer()
-        }
-        hideSystemUi()
+    private fun reloadVideo(){
+        val currentPosition = player?.getCurrentTime()
+        val tpImp = player as TpStreamPlayerImpl
+        tpImp.load(player?.videoInfo?.getPlaybackURL()!!,currentPosition!!)
     }
 
-    private fun hideSystemUi() {
-        activity?.let { activity ->
-            WindowCompat.setDecorFitsSystemWindows(activity.window, false)
-            WindowInsetsControllerCompat(activity.window, viewBinding.videoView).let { controller ->
-                controller.hide(WindowInsetsCompat.Type.systemBars())
-                controller.systemBarsBehavior =
-                    WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-            }
-            Log.d(TAG, "hideSystemUi: ")
-        }
-    }
-
-    private fun initializePlayer() {
-        _player = initializeExoplayer()
-        player = TpStreamPlayerImpl(_player!!, requireContext())
-        this.initializationListener?.onInitializationSuccess(player!!)
-    }
-
-    private fun initializeExoplayer(): ExoPlayer {
-        return ExoPlayer.Builder(requireActivity())
-            .setTrackSelector(trackSelector)
-            .build()
-            .also { exoPlayer ->
-                viewBinding.videoView.player = exoPlayer
-                exoPlayer.setAudioAttributes(AudioAttributes.DEFAULT, true)
-                exoPlayer.addListener(_playbackStateListener)
-            }
+    private fun storeCurrentPlayTime(){
+        startPosition = player?.getCurrentTime()?.div(1000L) ?: -1L
     }
 
     override fun onPause() {
@@ -357,28 +351,34 @@ class TpStreamPlayerFragment : Fragment(), DownloadCallback.Listener {
         Log.d(TAG, "onStop: ")
     }
 
-    private fun storeCurrentPlayTime(){
-        startPosition = player?.getCurrentTime()?.div(1000L) ?: -1L
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _viewBinding = null
     }
 
-    fun load(parameters: TpInitParams) {
-        if (player == null) {
-            throw Exception("Player is not initialized yet. `load` method should be called onInitializationSuccess")
-        }
-        if (startPosition != -1L){
-            parameters.startAt = startPosition
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        disableAutoFullScreenOnRotate()
+    }
 
-        player?.load(parameters) { exception ->
-            requireActivity().runOnUiThread {
-                viewBinding.errorMessage.visibility = View.VISIBLE
-                viewBinding.errorMessage.text = "Error Occurred while playing video. Error code ${exception.errorMessage}.\n ID: ${parameters.videoId}"
-                SentryLogger.logAPIException(exception,parameters)
+    fun enableAutoFullScreenOnRotate() {
+        orientationEventListener = OrientationListener(requireContext())
+        orientationEventListener.setOnChangeListener { isLandscape ->
+            activity?.let {
+                if(isLandscape) {
+                    showFullScreen()
+                } else {
+                    exitFullScreen()
+                }
             }
         }
-        player?.setPlayWhenReady(parameters.autoPlay==true)
-        showDownloadButton = parameters.isDownloadEnabled
-        updateDownloadButtonImage(parameters)
+        orientationEventListener.start()
+    }
+
+    fun disableAutoFullScreenOnRotate() {
+        if(::orientationEventListener.isInitialized) {
+            orientationEventListener.disable()
+        }
     }
 
     inner class PlayerListener : Player.Listener, DRMLicenseFetchCallback {

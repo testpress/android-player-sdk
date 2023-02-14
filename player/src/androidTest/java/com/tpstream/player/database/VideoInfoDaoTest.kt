f2c2caf281c23415d96b361a18aad51d0266b955
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-02-14 11:39:55 +0530

    refactor

--- player/src/androidTest/java/com/tpstream/player/database/VideoInfoDaoTest.kt
+++ player/src/androidTest/java/com/tpstream/player/database/VideoInfoDaoTest.kt
@@ -1,6 +1,6 @@
 package com.tpstream.player.database
 
-import androidx.lifecycle.Observer
+import android.app.usage.UsageEvents
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.tpstream.player.models.OfflineVideoInfo
 import com.tpstream.player.models.OfflineVideoState
@@ -9,8 +9,8 @@ import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import com.tpstream.player.util.getOrAwaitValue
 import org.junit.runner.RunWith
-import org.mockito.Mock
 
 import java.util.*
 
@@ -19,17 +19,16 @@ import java.util.*
 class OfflineVideoInfoDaoTest : TPStreamsDatabaseTest() {
 
     @Test
-    fun testAddData() = runBlocking {
-        val offlineVideoInfo = createData()
-        db.offlineVideoInfoDao().insert(offlineVideoInfo)
-
-        val fetchOfflineVideoInfo = db.offlineVideoInfoDao().getOfflineVideoInfoByVideoId("Demo")
-        assertThat(fetchOfflineVideoInfo?.videoId, equalTo(offlineVideoInfo.videoId))
+    fun testInsert() = runBlocking {
+        val videoID = "Demo_1"
+        addDataToDB()
+        val fetchOfflineVideoInfo = db.offlineVideoInfoDao().getOfflineVideoInfoByVideoId(videoID)
+        assertThat(fetchOfflineVideoInfo?.videoId, equalTo(videoID))
     }
 
     @Test
-    fun testDeleteData() = runBlocking {
-        val offlineVideoInfo = createData()
+    fun testDelete() = runBlocking {
+        val offlineVideoInfo = createData(1L)
         //Adding offlineVideoInfo
         db.offlineVideoInfoDao().insert(offlineVideoInfo)
         assertTrue(db.offlineVideoInfoDao().getAllOfflineVideoInfo()!!.isNotEmpty())
@@ -40,55 +39,64 @@ class OfflineVideoInfoDaoTest : TPStreamsDatabaseTest() {
 
     @Test
     fun testGetOfflineVideoInfoByVideoId() = runBlocking {
-        val offlineVideoInfo = createData()
-        db.offlineVideoInfoDao().insert(offlineVideoInfo)
-
-        val fetchOfflineVideoInfoByVideoID = db.offlineVideoInfoDao().getOfflineVideoInfoByVideoId("Demo")
-        assertThat(fetchOfflineVideoInfoByVideoID?.videoId, equalTo(offlineVideoInfo.videoId))
+        val videoID = "Demo_2"
+        addDataToDB()
+        val fetchOfflineVideoInfoByVideoID = db.offlineVideoInfoDao().getOfflineVideoInfoByVideoId(videoID)
+        assertThat(fetchOfflineVideoInfoByVideoID?.videoId, equalTo(videoID))
     }
 
     @Test
     fun testGetOfflineVideoInfoByUrl() = runBlocking {
-        val offlineVideoInfo = createData()
-        db.offlineVideoInfoDao().insert(offlineVideoInfo)
-
-        val fetchOfflineVideoInfoByUrl = db.offlineVideoInfoDao().getOfflineVideoInfoByUrl("playback_url")
-        assertThat(fetchOfflineVideoInfoByUrl?.url, equalTo(offlineVideoInfo.url))
+        val url = "playback_url_2"
+        addDataToDB()
+        val fetchOfflineVideoInfoByUrl = db.offlineVideoInfoDao().getOfflineVideoInfoByUrl(url)
+        assertThat(fetchOfflineVideoInfoByUrl?.url, equalTo(url))
     }
 
     @Test
     fun testGetAllVideoInfo() = runBlocking {
+        addDataToDB()
+        assertThat(db.offlineVideoInfoDao().getAllOfflineVideoInfo()!!.size, equalTo(5))
+    }
+
+    @Test
+    fun testGetAllDownloadInLiveData() = runBlocking {
+        addDataToDB()
+        val fetchedContent = db.offlineVideoInfoDao().getAllDownloadInLiveData().getOrAwaitValue()
+        assertThat(fetchedContent?.size, equalTo(5))
+    }
+
+    @Test
+    fun testGetOfflineVideoInfoById() = runBlocking {
+        val videoID = "Demo_5"
+        addDataToDB()
+        val fetchOfflineVideoInfoByID = db.offlineVideoInfoDao().getOfflineVideoInfoById(videoID).getOrAwaitValue()
+        assertThat(fetchOfflineVideoInfoByID?.videoId, equalTo(videoID))
+    }
+
+    private fun addDataToDB() = runBlocking {
         val data1 = createData(1L)
         val data2 = createData(2L)
         val data3 = createData(3L)
+        val data4 = createData(4L)
+        val data5 = createData(5L)
 
         db.offlineVideoInfoDao().insert(data1)
         db.offlineVideoInfoDao().insert(data2)
         db.offlineVideoInfoDao().insert(data3)
-
-        assertThat(db.offlineVideoInfoDao().getAllOfflineVideoInfo()!!.size, equalTo(3))
+        db.offlineVideoInfoDao().insert(data4)
+        db.offlineVideoInfoDao().insert(data5)
     }
-
-//    @Mock
-//    private val observer :Observer<List<OfflineVideoInfo>?> = Observer<List<OfflineVideoInfo>?>()
-
-//    @Test
-//    fun lie() = runBlocking {
-//        val offlineVideoInfo = createData()
-//        db.offlineVideoInfoDao().getAllDownloadInLiveData().observeForever(observer)
-//        db.offlineVideoInfoDao().insert(offlineVideoInfo)
-//        verify(observer).onChanged(Collections.singletonList(offlineVideoInfo))
-//    }
 
     private fun createData(count: Long = 0L): OfflineVideoInfo {
         return OfflineVideoInfo(
-            1+count,
-            "Demo",
-            "title $count",
-            "thumbnail",
-            "playback_url",
+            0+count,
+            "Demo_$count",
+            "title_$count",
+            "thumbnail_$count",
+            "playback_url_$count",
             "10:00:00",
-            "description",
+            "description_$count",
             "4",
             100,
             100000,

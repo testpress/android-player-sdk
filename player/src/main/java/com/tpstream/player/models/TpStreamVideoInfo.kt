d19cc2c87f05153036ba59d62ff73a02ef8c0398
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-03-01 10:52:43 +0530

    refactor

--- player/src/main/java/com/tpstream/player/models/TpStreamVideoInfo.kt
+++ player/src/main/java/com/tpstream/player/models/TpStreamVideoInfo.kt
@@ -1,26 +1,26 @@
 package com.tpstream.player.models
 
-data class Video(
-    val progress: Int?,
-    val thumbnails: Array<String>?,
-    val status: String?,
-    val playback_url: String?,
-    val dash_url: String?,
-    val preview_thumbnail_url: String?,
-    val format: String?,
-    val resolutions: Array<String>?,
-    val video_codec: String?,
-    val audio_codec: String?,
-    val enable_drm: Boolean?
-)
-
-data class TpStreamVideoInfo(
+internal data class TpStreamVideoInfo(
     val id: String?,
     val title: String?,
     val bytes: Long?,
     val type: String?,
     val video: Video?
 ) {
+
+    data class Video(
+        val progress: Int?,
+        val thumbnails: Array<String>?,
+        val status: String?,
+        val playback_url: String?,
+        val dash_url: String?,
+        val preview_thumbnail_url: String?,
+        val format: String?,
+        val resolutions: Array<String>?,
+        val video_codec: String?,
+        val audio_codec: String?,
+        val enable_drm: Boolean?
+    )
 
     fun asVideoInfo():VideoInfo{
         return VideoInfo(
@@ -29,7 +29,7 @@ data class TpStreamVideoInfo(
             thumbnailSmall = "",
             thumbnailMedium = "",
             url = this.video?.playback_url,
-            dashUrl = this.video?.dash_url,
+            dashUrl = if (this.video?.enable_drm == true) this.video.dash_url else null,
             hlsUrl = "",
             duration = "",
             description = "",

c1e9f7ea0f8076ab50b92fe6d6cb7a58c8b19b0d
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-03-02 14:00:08 +0530

    refactor

--- player/src/main/java/com/tpstream/player/database/roomMigration/RoomMigration2To3.kt
+++ player/src/main/java/com/tpstream/player/database/roomMigration/RoomMigration2To3.kt
@@ -15,7 +15,7 @@ internal object RoomMigration2To3 {
     }
 
     private fun createNewTable(): String {
-        return "CREATE TABLE IF NOT EXISTS `OfflineVideoInfoTemp` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `videoId` TEXT NOT NULL, `title` TEXT NOT NULL, `thumbnail` TEXT NOT NULL, `url` TEXT NOT NULL, `duration` TEXT NOT NULL, `description` TEXT NOT NULL, `transcodingStatus` TEXT NOT NULL, `percentageDownloaded` INTEGER NOT NULL, `bytesDownloaded` INTEGER NOT NULL, `totalSize` INTEGER NOT NULL, `downloadState` TEXT, `videoWidth` INTEGER NOT NULL, `videoHeight` INTEGER NOT NULL, `params` TEXT NOT NULL)"
+        return "CREATE TABLE IF NOT EXISTS `OfflineVideoInfoTemp` (`id` INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, `videoId` TEXT NOT NULL, `title` TEXT NOT NULL, `thumbnail` TEXT NOT NULL, `url` TEXT NOT NULL, `duration` TEXT NOT NULL, `description` TEXT NOT NULL, `transcodingStatus` TEXT NOT NULL, `percentageDownloaded` INTEGER NOT NULL, `bytesDownloaded` INTEGER NOT NULL, `totalSize` INTEGER NOT NULL, `downloadState` TEXT, `videoWidth` INTEGER NOT NULL, `videoHeight` INTEGER NOT NULL, `params` TEXT NOT NULL DEFAULT '')"
     }
 
     private fun copyDataFromOldToNewTable(): String {

a881bde8923a60677d74f67dc3431bce6ed7dcfe
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-03-27 13:12:53 +0530

    chore: Remove dead code

--- player/src/main/java/com/tpstream/player/TpStreamPlayerViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.tpstream.player
-
-import androidx.lifecycle.ViewModel
-
-class TpStreamPlayerViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
--- player/src/main/java/com/tpstream/player/models/DRMLicenseURL.kt
+++
@@ -1,8 +0,0 @@
-package com.tpstream.player.models
-
-import com.google.gson.annotations.SerializedName
-
-data class DRMLicenseURL(
-    @SerializedName("license_url")
-    val licenseUrl: String?
-)

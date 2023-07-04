1a744fd4d758c5552d941dfbc4f76a94ce5e96b0
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-07-04 15:08:07 +0530

    add seekbar disable and enable option

--- player/src/main/java/com/tpstream/player/TpStreamPlayer.kt
+++ player/src/main/java/com/tpstream/player/TpStreamPlayer.kt
@@ -215,6 +215,7 @@ internal class TpStreamPlayerImpl(val context: Context) : TpStreamPlayer {
 }
 
 internal interface TpStreamPlayerImplCallBack {
+    fun updateDownloadButton(showDownloadButton: Boolean,videoId: String)
     fun onPlaybackError(parameters: TpInitParams,exception: TPException)
     fun onPlayerPrepare()
 }

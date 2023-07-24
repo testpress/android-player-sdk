85a3061f21bc4dc8f2b954624cd732b62a4366a1
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-07-24 12:42:52 +0530

    refcator

--- player/src/main/java/com/tpstream/player/ui/TPStreamPlayerView.kt
+++ player/src/main/java/com/tpstream/player/ui/TPStreamPlayerView.kt
@@ -6,14 +6,9 @@ import android.content.Context
 import android.content.DialogInterface
 import android.content.res.ColorStateList
 import android.graphics.Color
-import android.os.Handler
-import android.os.Looper
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewTreeObserver
-import android.view.animation.TranslateAnimation
 import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.TextView
@@ -45,8 +40,6 @@ import com.tpstream.player.util.MarkerState
 import com.tpstream.player.util.getPlayedStatusArray
 import java.util.*
 import java.util.concurrent.TimeUnit
-
-private const val ONE_SECONDS_DELAY: Long = 1000
 
 class TPStreamPlayerView @JvmOverloads constructor(
     context: Context,

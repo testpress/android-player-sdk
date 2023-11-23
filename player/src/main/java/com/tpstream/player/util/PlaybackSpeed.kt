0a163f65d428d33af6f7888efd409d260f1acdbc
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-11-23 11:07:19 +0530

    fic

---
+++ player/src/main/java/com/tpstream/player/util/PlaybackSpeed.kt
@@ -0,0 +1,11 @@
+package com.tpstream.player.util
+
+internal enum class Speed(val value: Float, val text: String) {
+    SPEED_0_25(0.25f, "0.25x"),
+    SPEED_0_5(0.5f, "0.5x"),
+    SPEED_0_75(0.75f, "0.75x"),
+    SPEED_1_0(1.0f, "1x"),
+    SPEED_1_25(1.25f, "1.25x"),
+    SPEED_1_5(1.5f, "1.5x"),
+    SPEED_2_0(2.0f, "2x")
+}

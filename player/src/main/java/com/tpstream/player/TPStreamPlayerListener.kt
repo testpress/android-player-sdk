2851a5b69757d9bbbbb18f85f25a30075fc620c2
PruthiviRaj27 <pruthiviraj@testpress.in>
2023-12-29 10:49:00 +0530

    fix

--- player/src/main/java/com/tpstream/player/TPStreamPlayerListener.kt
+++ player/src/main/java/com/tpstream/player/TPStreamPlayerListener.kt
@@ -4,9 +4,11 @@ import com.tpstream.player.enum.PlaybackError
 
 interface TPStreamPlayerListener {
     fun onTracksChanged(tracks: Tracks) {}
+    @Deprecated("Deprecated",level = DeprecationLevel.WARNING)
     fun onMetadata(metadata: Metadata) {}
     fun onIsPlayingChanged(playing: Boolean) {}
     fun onIsLoadingChanged(loading: Boolean) {}
+    @Deprecated("Deprecated",level = DeprecationLevel.WARNING)
     fun onDeviceInfoChanged(deviceInfo: DeviceInfo) {}
     fun onPlayWhenReadyChanged(playWhenReady: Boolean, reason: Int) {}
     fun onEvents(player: TpStreamPlayer?, events: PlayerEvents) {}
@@ -14,6 +16,7 @@ interface TPStreamPlayerListener {
     fun onSeekForwardIncrementChanged(seekForwardIncrementMs: Long) {}
     fun onVideoSizeChanged(videoSize: VideoSize) {}
     fun onPositionDiscontinuity(oldPosition: PlayerPositionInfo, newPosition: PlayerPositionInfo, reason: Int) {}
+    @Deprecated("Deprecated", ReplaceWith("TPStreamPlayerListener.onPlayerError()"), DeprecationLevel.WARNING)
     fun onPlayerErrorChanged(error: PlaybackException?) {}
     fun onTimelineChanged(timeline: Timeline, reason: Int) {}
     fun onPlaybackStateChanged(playbackState: Int) {}

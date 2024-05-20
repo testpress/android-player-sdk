e453cc3e06f02197e47153bc152c290f006851e7
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-05-20 16:52:07 +0530

    refcator

--- player/src/main/java/com/tpstream/player/ui/TPStreamPlayerView.kt
+++ player/src/main/java/com/tpstream/player/ui/TPStreamPlayerView.kt
@@ -7,7 +7,6 @@ import android.content.DialogInterface
 import android.content.res.ColorStateList
 import android.graphics.Color
 import android.util.AttributeSet
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.widget.*

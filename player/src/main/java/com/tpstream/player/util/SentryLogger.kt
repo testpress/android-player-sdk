58a52d55458ab0c8828a9af6700b2ce6aa62a422
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-07-26 18:37:40 +0530

    refcator

--- player/src/main/java/com/tpstream/player/util/SentryLogger.kt
+++ player/src/main/java/com/tpstream/player/util/SentryLogger.kt
@@ -26,6 +26,20 @@ internal object SentryLogger {
             scope.setTag("TPStreamsAndroidPlayerSDKVersion",TPSTREAMS_ANDROID_PALYER_SDK_VERSION_NAME)
             scope.setTag("playerId", playerId)
             scope.setTag("userId", params?.userId ?: "")
+            scope.setContexts(
+                "TPStreamsSDK",
+                mapOf(
+                    "SDK Version" to TPSTREAMS_ANDROID_PALYER_SDK_VERSION_NAME,
+                    "Player Id" to playerId,
+                    "Error Type" to "Server error",
+                    "Error Code" to exception.response?.code,
+                    "Error Message" to exception.errorMessage,
+                    "Org Code" to TPStreamsSDK.orgCode,
+                    "Video ID" to params?.videoId,
+                    "AccessToken" to params?.accessToken,
+                    "userId" to params?.userId,
+                )
+            )
         }
     }
 
@@ -42,12 +56,26 @@ internal object SentryLogger {
             scope.setTag("TPStreamsAndroidPlayerSDKVersion",TPSTREAMS_ANDROID_PALYER_SDK_VERSION_NAME)
             scope.setTag("playerId", playerId)
             scope.setTag("userId", params?.userId ?: "")
+            scope.setContexts(
+                "TPStreamsSDK",
+                mapOf(
+                    "SDK Version" to TPSTREAMS_ANDROID_PALYER_SDK_VERSION_NAME,
+                    "Player Id" to playerId,
+                    "Error Type" to "Player error",
+                    "Error Code" to error.errorCode,
+                    "Error Message" to error.errorCodeName,
+                    "Org Code" to TPStreamsSDK.orgCode,
+                    "Video ID" to params?.videoId,
+                    "AccessToken" to params?.accessToken,
+                    "userId" to params?.userId,
+                )
+            )
         }
     }
 
     fun logDrmSessionException(error: DrmSessionException, params: TpInitParams?, playerId: String) {
         Sentry.captureMessage(
-            "Player error" +
+            "Player DRM error" +
                     " Code: ${error.errorCode}" +
                     " Message: ${error.message}" +
                     " Video ID: ${params?.videoId}" +
@@ -57,6 +85,20 @@ internal object SentryLogger {
             scope.setTag("TPStreamsAndroidPlayerSDKVersion",TPSTREAMS_ANDROID_PALYER_SDK_VERSION_NAME)
             scope.setTag("playerId", playerId)
             scope.setTag("userId", params?.userId ?: "")
+            scope.setContexts(
+                "TPStreamsSDK",
+                mapOf(
+                    "SDK Version" to TPSTREAMS_ANDROID_PALYER_SDK_VERSION_NAME,
+                    "Player Id" to playerId,
+                    "Error Type" to "Player DRM error",
+                    "Error Code" to error.errorCode,
+                    "Error Message" to error.message,
+                    "Org Code" to TPStreamsSDK.orgCode,
+                    "Video ID" to params?.videoId,
+                    "AccessToken" to params?.accessToken,
+                    "userId" to params?.userId,
+                )
+            )
         }
     }
 }

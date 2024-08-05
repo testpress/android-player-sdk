28ee84bddf6365a3128db8a0ed64d6014da959bc
PruthiviRaj <112844543+PruthiviRaj27@users.noreply.github.com>
2024-08-05 15:40:39 +0530

    chore: Remove unused method (#214)

--- player/src/main/java/com/tpstream/player/util/NetworkClient.kt
+++ player/src/main/java/com/tpstream/player/util/NetworkClient.kt
@@ -22,29 +22,12 @@ internal class NetworkClient<T : Any>(val klass: Class<T>) {
     private var client: OkHttpClient = OkHttpClient();
     private val gson = Gson()
 
-    fun get(url: String, callback: TPResponse<T>? = null): T? {
+    fun get(url: String, callback: TPResponse<T>) {
         val request = Request.Builder().url(URL(url)).build()
-        return makeRequest(callback, request)
+        return makeRequest(request, callback)
     }
 
-    fun post(url: String, body: RequestBody, callback: TPResponse<T>? = null): T? {
-        val request = Request.Builder().url(URL(url)).post(body).build()
-        return makeRequest(callback, request)
-    }
-
-    private fun makeRequest(
-        callback: TPResponse<T>?,
-        request: Request
-    ): T? {
-        return if (callback != null) {
-            makeAsyncRequest(request, callback)
-            null
-        } else {
-            makeSyncRequest(request)
-        }
-    }
-
-    private fun makeAsyncRequest(request: Request, callback: TPResponse<T>) {
+    private fun makeRequest(request: Request, callback: TPResponse<T>) {
         client.newCall(request).enqueue(object: Callback {
             override fun onResponse(call: Call, response: Response) {
                 if (response.isSuccessful){
@@ -67,14 +50,6 @@ internal class NetworkClient<T : Any>(val klass: Class<T>) {
                 callback.onFailure(TPException.networkError(e))
             }
         })
-    }
-
-    private fun makeSyncRequest(request: Request): T? {
-        val response = client.newCall(request).execute()
-        if (response.isSuccessful) {
-            return gson.fromJson(response.body?.charStream(), klass)
-        }
-        throw TPException.httpError(response)
     }
 
     interface TPResponse<T> {

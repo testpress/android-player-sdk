5cdf1007d92422c655bc8ec73c481bd6c4bfc061
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-09-11 13:32:15 +0530

    refcator

--- app/src/main/java/com/tpstream/app/PlayerActivity.kt
+++ app/src/main/java/com/tpstream/app/PlayerActivity.kt
@@ -101,9 +101,10 @@ class PlayerActivity : AppCompatActivity() {
                 orgCode = "lmsdemo"
                 provider = TPStreamsSDK.Provider.TestPress
             }
+            // https://app.tpstreams.com/embed/6eafqn/7RKQZj4gB2T/?access_token=d4986429-20e2-4b21-93ae-c70630a37e06
             "TPS_DRM" -> {
-                accessToken = "ab70caed-6168-497f-89c1-1e308da2c9aa"
-                videoId = "6suEBPy7EG4"
+                accessToken = "d4986429-20e2-4b21-93ae-c70630a37e06"
+                videoId = "7RKQZj4gB2T"
                 orgCode = "6eafqn"
                 provider = TPStreamsSDK.Provider.TPStreams
             }

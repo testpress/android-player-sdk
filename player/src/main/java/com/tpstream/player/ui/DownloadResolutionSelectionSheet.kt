0b79767258998bd4e4ea6c8ee89888f1427d45e3
PruthiviRaj27 <pruthiviraj@testpress.in>
2024-09-27 19:32:30 +0530

    refcator

--- player/src/main/java/com/tpstream/player/ui/DownloadResolutionSelectionSheet.kt
+++ player/src/main/java/com/tpstream/player/ui/DownloadResolutionSelectionSheet.kt
@@ -17,6 +17,8 @@ import com.tpstream.player.*
 import com.tpstream.player.R
 import com.tpstream.player.databinding.TpDownloadTrackSelectionDialogBinding
 import com.tpstream.player.data.Asset
+import com.tpstream.player.data.Track
+import com.tpstream.player.data.source.network.TPStreamsNetworkAsset
 import com.tpstream.player.offline.VideoDownloadRequestCreationHandler
 import com.tpstream.player.util.DeviceUtil
 import okio.IOException
@@ -229,13 +231,22 @@ internal class DownloadResolutionSelectionSheet : BottomSheetDialogFragment(), V
 
         private fun getTotalMediaSize(trackInfo: TrackInfo): String {
             val track = asset.video.tracks?.firstOrNull { it.type == "Playlist" }
-
-            val playlistSize = track?.playlists
-                ?.firstOrNull { it.height == trackInfo.videoFormat.height }
-                ?.bytes?.let { it / 1024L / 1024L }
-
+            val playlistSize = getPlaylistSize(track, trackInfo)
             val size = playlistSize ?: (getVideoSizeInMB(trackInfo) + getAudioSizeInMB(trackInfo))
             return "$size MB"
+        }
+
+        private fun getPlaylistSize(track: Track?, trackInfo: TrackInfo): Long? {
+            return track?.playlists
+                ?.filter { // Filter based on DRM protection
+                    if (asset.video.isDrmProtected == true) {
+                        it.name.contains("dash")
+                    } else {
+                        true // No filtering for non-DRM, allow all playlists
+                    }
+                }
+                ?.firstOrNull { it.height == trackInfo.videoFormat.height }
+                ?.bytes?.let { it / 1024L / 1024L }
         }
 
         private fun getVideoSizeInMB(trackInfo: TrackInfo): Int {

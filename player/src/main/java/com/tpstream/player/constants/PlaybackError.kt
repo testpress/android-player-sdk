caebc4cbf9552bdbc54bee543b5362557a088f0e
PruthiviRaj <112844543+PruthiviRaj27@users.noreply.github.com>
2025-02-07 10:42:32 +0530

    fix: Display custom message for Error code 4003 (#242)
    - Previously, we displayed a generic error message for ExoPlayer error code 4003. This error typically occurs due to insufficient system resources, missing or incompatible decoder libraries, or similar issues. Restarting the device usually resolves it.
    - We added a custom error message specifically for error code 4003.
    - Included a troubleshooting link within the message for further assistance.

--- player/src/main/java/com/tpstream/player/constants/PlaybackError.kt
+++ player/src/main/java/com/tpstream/player/constants/PlaybackError.kt
@@ -50,6 +50,7 @@ internal fun PlaybackException.getErrorMessage(playerId: String): String {
         PlaybackException.ERROR_CODE_IO_NETWORK_CONNECTION_TIMEOUT -> "The request took too long to process due to a slow or unstable network connection. Please try again.\n Player code: ${this.errorCode}. Player Id: $playerId"
         PlaybackException.ERROR_CODE_DRM_LICENSE_ACQUISITION_FAILED -> "There was an issue fetching the license key for this video. Please try again later.\n Player code: ${this.errorCode}. Player Id: $playerId"
         PlaybackException.ERROR_CODE_DECODER_INIT_FAILED -> "<html><body><p>An error occurred while playing the video. Try restarting your device or playing another video. More help <a href='https://tpstreams.com/help/troubleshooting-steps-for-error-code-4001'>click here</a>.<br> Player code: ${this.errorCode}. Player Id: $playerId</p></body></html>"
+        PlaybackException.ERROR_CODE_DECODING_FAILED -> "<html><body><p>An error occurred while playing the video. Try restarting your device or selecting a different resolution. More help <a href='https://tpstreams.com/help/troubleshooting-steps-for-error-code-4001'>click here</a>.<br> Player code: ${this.errorCode}. Player Id: $playerId</p></body></html>"
         else -> "Oops! Something went wrong. Please contact support for assistance and provide details about the issue.\n Player code: ${this.errorCode}. Player Id: $playerId"
     }
 }
